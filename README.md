7b223a4df065d12bc2b9207347fbd6c02a145c2e
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-10-16 15:37:31 -0400

    Remove clutter filesd

--- README.md
+++
@@ -1,7 +0,0 @@
-# Typeracer API
-
-LOMO Coding Club presents...
-
-A typeracer clone
-
-Run `heroku local web` or press the run button in VSCode and then open `http://localhost:8080` in the browser
--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.nodejs-16_x
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
